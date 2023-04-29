c15ae5899990d1f096814d6b1fe401d3e45e00f5
MD TAUFIK <110840076+Huntertk@users.noreply.github.com>
2023-04-29 23:49:08 +0530

    Third Commit  Adding Adding Product now User Can Add Item to the Card

--- Pages/Cart.jsx
+++ Pages/Cart.jsx
@@ -1,28 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext,useEffect } from 'react'
 import { BsTrash,BsFillTrashFill } from "react-icons/bs";
 import './cart.css'
+import Aos from 'aos';
+import 'aos/dist/aos.css'
 import { Context } from '../Components/Context'
 const Cart = () => {
   const context = useContext(Context)
-  console.log(context.cartItems)
 
   const renderCart = context.cartItems.map((item) => {
-    return <div key={item.id} className="cart-item-container">
+    return <div key={item.id} className="cart-item-container"
+            data-aos="fade-up"
+            data-aos-anchor-placement="center-bottom"
+    >
             <span className='name'>{item.name}</span>
             <div className='botton-cart-container'>
               <img className='cart-img' src={item.img} alt={item.name} />
               <span className='price'>₹ {item.price}</span>
-              <BsTrash />
+              <BsTrash onClick={() => {
+                context.removeCart(item.id)}}/>
             </div>
           </div>
   })
  
+
+   useEffect(() => {
+    Aos.init()
+  },[])
   return (
     <div className='cart-container'>
+      <img src="https://cdn.shopify.com/s/files/1/0057/8938/4802/files/551-Desktop_2_1440x.jpg?v=1682661291" alt="" className='background-img'/>
       <h1>Cart</h1>
 
-    { context.cartItems.length > 0 ? renderCart  : <h1>Your Cart Is Empty</h1>
-    }
+    { context.cartItems.length > 0 ? renderCart  : <h1>Your Cart Is Empty</h1>}
     
     
     <h3>Total : ₹ 0</h3> 
